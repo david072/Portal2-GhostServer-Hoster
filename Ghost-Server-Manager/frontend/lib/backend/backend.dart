a2b37ce34de95725eea8152636b18a7277262517
David Ganz <david.g.ganz@gmail.com>
2025-10-30 16:24:29 +0100

    chore(frontend): reimplement basic home page

--- Ghost-Server-Manager/frontend/lib/backend/backend.dart
+++ Ghost-Server-Manager/frontend/lib/backend/backend.dart
@@ -1,20 +1,75 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:http/http.dart' as http;
+import 'package:portal2_ghost_sever_hoster/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-const _baseUri = "http://localhost:8080";
+part 'backend.freezed.dart';
+
+part 'backend.g.dart';
+
+const _host = "localhost";
+const _baseUri = "http://$_host:8080";
 const _baseAuthUri = "$_baseUri/auth";
+const _baseApiUri = "$_baseUri/api";
 
 typedef Json = Map<String, dynamic>;
 
-class Backend {
-  static Future<http.Response> _postJson(String uri, Json json) => http.post(
+@freezed
+abstract class GhostServer with _$GhostServer {
+  const GhostServer._();
+
+  const factory GhostServer({
+    required int id,
+    required String containerId,
+    required int port,
+    required int wsPort,
+    required int userId,
+    required String name,
+    required String relativeRemainingDuration,
+  }) = _GhostServer;
+
+  factory GhostServer.fromJson(Map<String, dynamic> json) =>
+      _$GhostServerFromJson(json);
+
+  String connectCommand() => "ghost_connect $_host $wsPort";
+}
+
+class _Backend {
+  const _Backend();
+
+  Future<String> _getAuthToken() async =>
+      (await SharedPreferences.getInstance()).getString(spAuthTokenKey) ??
+      (throw "Please log in!");
+
+  Future<http.Response> _postJson(
+    String uri,
+    Json json, {
+    bool authenticated = false,
+  }) async => http.post(
     Uri.parse(uri),
-    headers: {"Content-Type": "application/json"},
+    headers: {
+      HttpHeaders.contentTypeHeader: "application/json",
+      if (authenticated)
+        HttpHeaders.authorizationHeader: "Bearer ${await _getAuthToken()}",
+    },
     body: jsonEncode(json),
   );
 
-  static Future<(String, DateTime)> login(String email, String password) async {
+  Future<http.Response> _get(
+    String uri, {
+    bool authenticated = false,
+  }) async => http.get(
+    Uri.parse(uri),
+    headers: {
+      if (authenticated)
+        HttpHeaders.authorizationHeader: "Bearer ${await _getAuthToken()}",
+    },
+  );
+
+  Future<(String, DateTime)> login(String email, String password) async {
     var response = await _postJson(
       "$_baseAuthUri/generateAuthToken2",
       {"email": email, "password": password},
@@ -27,11 +82,24 @@ class Backend {
     );
   }
 
-  static Future<void> register(String email, String password) async {
+  Future<void> register(String email, String password) async {
     var response = await _postJson(
       "$_baseAuthUri/register",
       {"email": email, "password": password},
     );
     if (response.statusCode != 201) throw response.body;
   }
+
+  Future<List<GhostServer>> getGhostServers() async {
+    var response = await _get("$_baseApiUri/list", authenticated: true);
+    if (response.statusCode != 200) throw response.body;
+    var json = jsonDecode(response.body) as List<dynamic>;
+    return json.cast<Json>().map(GhostServer.fromJson).toList();
+  }
+
+  Future<void> deleteGhostServer(int id) =>
+      _get("$_baseApiUri/delete?id=$id", authenticated: true);
 }
+
+// ignore: constant_identifier_names
+const Backend = _Backend();
--- Ghost-Server-Manager/frontend/lib/pages/home_page.dart
+++ Ghost-Server-Manager/frontend/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:portal2_ghost_sever_hoster/backend/backend.dart';
 import 'package:portal2_ghost_sever_hoster/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,19 +15,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool loading = true;
 
+  List<GhostServer> servers = [];
+
   @override
   void initState() {
     super.initState();
     setup();
   }
 
-  Future<void> setup() async {
+  Future<bool> checkLoggedIn() async {
     var sp = await SharedPreferences.getInstance();
     var authToken = sp.getString(spAuthTokenKey);
     if (authToken == null) {
-      if (!mounted) return;
-      context.go("/login");
-      return;
+      return false;
     }
 
     var expiry = DateTime.fromMillisecondsSinceEpoch(
@@ -35,11 +37,20 @@ class _HomePageState extends State<HomePage> {
         DateTime.now().isAfter(expiry)) {
       sp.remove(spAuthTokenKey);
       sp.remove(spAuthTokenExpiryKey);
+      return false;
+    }
 
+    return true;
+  }
+
+  Future<void> setup() async {
+    if (!await checkLoggedIn()) {
       if (!mounted) return;
       context.go("/login");
       return;
     }
+
+    servers = await Backend.getGhostServers();
 
     setState(() => loading = false);
   }
@@ -47,9 +58,139 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: !loading
-          ? const Center(child: Text("Hello, World!"))
-          : const Center(child: CircularProgressIndicator()),
+      appBar: AppBar(
+        title: const Text("Ghost Servers"),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: setup, icon: const Icon(Icons.refresh)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: !loading
+            ? Center(
+                child: SizedBox(
+                  width: MediaQuery.sizeOf(context).width / 2,
+                  child: ListView.builder(
+                    itemCount: servers.length,
+                    itemBuilder: (context, i) =>
+                        _GhostServerCard(server: servers[i], update: setup),
+                  ),
+                ),
+              )
+            : const Center(child: CircularProgressIndicator()),
+      ),
+    );
+  }
+}
+
+class _GhostServerCard extends StatelessWidget {
+  const _GhostServerCard({required this.server, required this.update});
+
+  final GhostServer server;
+  final void Function() update;
+
+  void copyConnectCommand(BuildContext context) {
+    Clipboard.setData(ClipboardData(text: server.connectCommand()));
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: const Text("Copied!")),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Text(
+                  server.name,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => _DeleteGhostServerDialog(
+                        server: server,
+                        update: update,
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.delete_outlined),
+                ),
+              ],
+            ),
+            const Divider(height: 20),
+            Text("Connecting:", style: Theme.of(context).textTheme.bodyLarge),
+            Text(
+              "To connect, paste the text below into your game's console and hit enter!",
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: TextEditingController(
+                text: server.connectCommand(),
+              ),
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  onPressed: () => copyConnectCommand(context),
+                  icon: const Icon(Icons.copy),
+                ),
+              ),
+              readOnly: true,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FilledButton.tonal(
+                  onPressed: () {},
+                  child: const Text("Webinterface"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DeleteGhostServerDialog extends StatelessWidget {
+  const _DeleteGhostServerDialog({required this.server, required this.update});
+
+  final GhostServer server;
+  final void Function() update;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Delete Ghost Server"),
+      content: Text(
+        "Do you really want to delete the Ghost Server "
+        "\"${server.name}\"? This cannot be reverted!",
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Cancel"),
+        ),
+        TextButton(
+          onPressed: () async {
+            await Backend.deleteGhostServer(server.id);
+            if (!context.mounted) return;
+            Navigator.pop(context);
+            update();
+          },
+          child: const Text("Delete"),
+        ),
+      ],
     );
   }
 }
